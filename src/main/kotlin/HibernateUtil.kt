4821d2d5963cfce78a760c2a873e5891e457baaf
fakechitor <ladasedanbaklajan06@gmail.com>
2024-11-11 23:55:40 +0300

    feat: add singleton object for sessionFactory

---
+++ src/main/kotlin/HibernateUtil.kt
@@ -0,0 +1,26 @@
+import model.Match
+import model.Player
+import org.hibernate.SessionFactory
+import org.hibernate.boot.MetadataSources
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder
+
+object HibernateUtil {
+    val sessionFactory = buildSessionFactory()
+
+    private fun buildSessionFactory(): SessionFactory {
+        try{
+            return  MetadataSources(StandardServiceRegistryBuilder().build())
+                .addAnnotatedClass(Match::class.java)
+                .addAnnotatedClass(Player::class.java)
+                .buildMetadata()
+                .buildSessionFactory()
+        }
+        catch(e: Throwable){
+            throw e
+        }
+    }
+
+    fun shutdown(){
+        sessionFactory.close()
+    }
+}
