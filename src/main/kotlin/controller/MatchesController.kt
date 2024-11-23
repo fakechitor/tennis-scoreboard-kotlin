8460e39eaec7bc1f3730436ca55058dc4063abbf
fakechiitor <ladasedanbaklajan06@gmail.com>
2024-11-24 01:19:14 +0300

    feat: add validation for /matches fields

--- src/main/kotlin/controller/MatchesController.kt
+++ src/main/kotlin/controller/MatchesController.kt
@@ -6,26 +6,36 @@ import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import service.FinishedMatchesPersistenceService
+import util.Validation
 
 private const val PATH_TO_JSP = "/matches.jsp"
 
 @WebServlet(urlPatterns = ["/matches"])
 class MatchesController : HttpServlet() {
     private val finishedMatches = FinishedMatchesPersistenceService()
+    private val validation = Validation()
 
     override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
-        val page = req.getParameter("page").toInt()
-        val name = req.getParameter("filter_by_player_name") ?: ""
-        //TODO make validation for page > 0
-        val matchesRequest = MatchesRequestDto(page , name)
-        val finishedMatches = finishedMatches.getMatches(matchesRequest)
-        // TODO make setAttribute more beautiful)
-        req.setAttribute("matches", finishedMatches)
-        req.setAttribute("additional", 5 * (page-1))
-        req.setAttribute("page" ,page.toString())
-        req.setAttribute("size", 5)
-        req.setAttribute("totalPages", 2)
-        req.setAttribute("filterName", name)
-        req.getRequestDispatcher(PATH_TO_JSP).forward(req, resp)
+        val page = req.getParameter("page")
+        val name = req.getParameter("filter_by_player_name")
+        try{
+            validation.validateMatchesAttributes(page, name)
+            val pageNumber = page.toInt()
+            val matchesRequest = MatchesRequestDto(pageNumber, name)
+            val finishedMatches = finishedMatches.getMatches(matchesRequest)
+            // TODO make setAttribute more beautiful)
+            req.setAttribute("matches", finishedMatches)
+            req.setAttribute("additional", 5 * (pageNumber - 1))
+            req.setAttribute("page", page)
+            req.setAttribute("size", 5)
+            req.setAttribute("totalPages", 2)
+            req.setAttribute("filterName", name)
+            req.getRequestDispatcher(PATH_TO_JSP).forward(req, resp)
+        }
+        catch (e: IllegalArgumentException){
+            e.printStackTrace()
+            resp.characterEncoding = "UTF-8"
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, e.message)
+        }
     }
 }
--- src/main/kotlin/util/Validation.kt
+++ src/main/kotlin/util/Validation.kt
@@ -1,4 +1,50 @@
 package util
 
+import java.io.InputStream
+
 class Validation {
+
+    fun validateMatchesAttributes(page: String, name: String){
+        checkNameContainsOnlyLetters(name)
+        checkForbiddenWord(name)
+        checkPageValueIsNumber(page)
+        checkPageValueIsPositive(page)
+    }
+
+    private fun checkPageValueIsPositive(page : String) {
+        if (page.toInt() <= 0){
+            throw IllegalArgumentException("Номер страницы должен быть больше 0")
+        }
+    }
+
+    private fun checkPageValueIsNumber(page: String) {
+        try{
+            page.toInt()
+        }
+        catch (e: NumberFormatException){
+            throw IllegalArgumentException("Номер страницы должен быть числом")
+        }
+    }
+
+    private fun checkNameContainsOnlyLetters(name: String){
+        for (item in name.lowercase()){
+            if (item !in 'а'..'я' && item !in 'a'..'z'){
+                throw IllegalArgumentException("Имя может содержать только буквы")
+            }
+        }
+    }
+
+    private fun checkForbiddenWord(name : String) {
+        val forbiddenWords = getForbiddenWords()
+        if (name in forbiddenWords){
+            throw IllegalArgumentException("Вы используете запрещенное имя. Попробуйте другое =)")
+        }
+    }
+
+    private fun getForbiddenWords(): List<String> {
+        val inputStream: InputStream? = this::class.java.classLoader.getResourceAsStream("ForbiddenWords")
+        return inputStream?.bufferedReader().use { reader ->
+            reader?.readText()?.split("\n")?.map { it.trim() } ?: emptyList()
+        }
+    }
 }
