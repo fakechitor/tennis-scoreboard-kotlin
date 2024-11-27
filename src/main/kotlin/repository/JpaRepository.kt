0b8ba3d31e459ae2f89c61fc3b12d3b70b1dad6e
fakechitor <ladasedanbaklajan06@gmail.com>
2024-11-27 03:08:04 +0300

    refactor: rewrite methods with .use

--- src/main/kotlin/repository/JpaRepository.kt
+++ src/main/kotlin/repository/JpaRepository.kt
@@ -1,7 +1,6 @@
 package repository
 
 import org.hibernate.Session
-import org.hibernate.Transaction
 import util.HibernateUtil
 
 abstract class JpaRepository {
@@ -10,33 +9,22 @@ abstract class JpaRepository {
     abstract fun <T> save(entity: T): T
 
     protected fun <T> execute(action: (Session) -> T): T {
-        var session: Session? = null
-        try {
-            session = sessionFactory.openSession()
+        sessionFactory.openSession().use { session ->
             return action(session)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            throw RuntimeException("Ошибка с доступом к базе данных")
-        } finally {
-            session?.close()
         }
     }
 
     protected fun <T> executeInTransaction(action: (Session) -> T): T {
-        var session: Session? = null
-        var transaction: Transaction? = null
-        try {
-            session = sessionFactory.openSession()
-            transaction = session.beginTransaction()
-            val result = action(session)
-            transaction.commit()
-            return result
-        } catch (e: Exception) {
-            transaction?.rollback()
-            e.printStackTrace()
-            throw RuntimeException("Ошибка с доступом к базе данных")
-        } finally {
-            session?.close()
+        sessionFactory.openSession().use { session ->
+            val transaction = session.beginTransaction()
+            return try {
+                val result = action(session)
+                transaction.commit()
+                result
+            } catch (e: Exception) {
+                transaction.rollback()
+                throw RuntimeException("Ошибка с доступом к базе данных", e)
+            }
         }
     }
 }
